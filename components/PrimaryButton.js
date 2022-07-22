5196abe65c4f216e8084873cd4d69a6b04f99a1d
OKORO KELVIN <okorokelvinemoakpooke@yahoo.com>
2022-07-22 21:46:14 +0100

    Updates

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -9,9 +9,7 @@ function PrimaryButton({ children }) {
     <View style={styles.buttonOuterContainer}>
       <Pressable
         style={({ pressed }) =>
-          pressed
-            ? [styles.buttonInnerContainer, styles.pressed]
-            : styles.buttonInnerContainer
+          pressed? [styles.buttonInnerContainer, styles.pressed]: styles.buttonInnerContainer
         }
         onPress={pressHandler}
         android_ripple={{ color: '#640233' }}
