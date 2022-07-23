0fe03ccf8a44a6e40dbfbeea33835f63a8437899
OKORO KELVIN <okorokelvinemoakpooke@yahoo.com>
2022-07-23 06:50:40 +0100

    Navigateing screen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 import PrimaryButton from '../components/PrimaryButton';
 
-function StartGameScreen() {
+function StartGameScreen({onPickNumber}) {
   const [enteredNumber, setEnteredNumber] = useState('');
 
   function numberInputHandler(enteredText) {
@@ -26,7 +26,7 @@ function StartGameScreen() {
       return;
     }
 
-    console.log('Valid number!');
+    onPickNumber(chosenNumber);
   }
 
   return (
